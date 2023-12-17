1a25211bef389e1aac3bd4970a3b6a07b8d1871f
brunojoao <brunokataklysm@gmail.com>
2023-12-16 23:23:16 -0300

    begin spring boot

---
+++ src/main/java/com/malucos/javalee/JavaleeApplication.java
@@ -0,0 +1,13 @@
+package com.malucos.javalee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malucos/javalee/JavaleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malucos.javalee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
